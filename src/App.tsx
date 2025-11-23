c104c91fe521b38c794320576e729783c6b5ee45
alex <alexstepanyak@gmail.com>
2025-11-23 22:54:15 +0200

    cleanup, init client side of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [text, setText] = useState('');
+    const token = localStorage.getItem('token');
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        console.log(text);
+    }, [text]);
+
+    return (
+        <>
+            <div>
+                <img src="../public/logo.png" className="logo react" alt="React logo"/>
+            </div>
+            <h1>Fake Info Verification Service - made by Oleksii Stepaniak, master of NU KhPI</h1>
+            <div className="card">
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <textarea
+                className="main-textarea"
+                value={text}
+                onChange={e => setText(e.target.value)}
+                placeholder={token ? "" : "To continue, you need to authenticate in our service!"}
+                disabled={!token}
+
+            ></textarea>
+        </>
+    )
 }
 
 export default App
